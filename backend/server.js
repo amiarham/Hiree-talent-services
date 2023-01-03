bc017a756f1f6733fb2b8fefe1e654cb535aa95a
Arham Shami <66700148+amiarham@users.noreply.github.com>
2023-01-03 21:46:42 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,11 @@ app.get("/api/config/google", (req, res) => {
   res.send(process.env.GOOGLE_API_KEY || "");
 });
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
+app.use(express.static(path.join(__dirname, '/../frontend')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../frontend/index.html'));
+});
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
