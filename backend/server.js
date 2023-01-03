304fb4d539c551c56eb664309e460e891a1073ad
Arham Shami <66700148+amiarham@users.noreply.github.com>
2023-01-03 22:12:55 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend')));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/./frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "/../frontend/build/index.html"))
 );
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
